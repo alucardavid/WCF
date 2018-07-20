849fad897ccdc3a94b733d6527e8496667c6d0d5
David Pereira <d_pereira@outlook.com.br>
2018-07-20 00:06:23 -0300

    Service  consuming

---
+++ ConsumindoServico/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConsumindoServico.ServiceReference1;
+
+namespace ConsumindoServico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            string cpf = txtCPF.Text;
+
+
+            try
+            {
+                ClienteServiceClient servico = new ClienteServiceClient();
+                Cliente clienteCadastro = new Cliente();
+
+                clienteCadastro.Nome = nome;
+                clienteCadastro.Cpf = cpf;
+
+                servico.Add(clienteCadastro);
+
+                MessageBox.Show("Cliente cadastrado com sucesso");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+
+            try
+            {
+                ClienteServiceClient servico = new ClienteServiceClient();
+
+                Cliente resultado = servico.Buscar(nome);
+
+                txtCPF.Text = resultado.Cpf;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
